952f3d09814df60d719d259ee749fa2fe533d487
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-07-07 20:53:07 -0400

    add in constants

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Nikhil Polepalli on 7/7/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+struct Constants{
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+}
+
